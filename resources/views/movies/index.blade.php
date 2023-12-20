a5d85e2164e1d006472aec57334420a4cb7883ad
DavideGila <davide.gila@gmail.com>
2023-12-20 15:38:52 +0100

    added cards to index.blade

---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title', 'All Books')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        @foreach ($movies as $movie)
+        <div class="col-3 gy-4">
+            <div class="card">
+                <img src="{{$movie->image}}" class="card-img-top" alt="">
+                <div class="card-body">
+                  <h5 class="card-title">{{$movie->title}}</h5>
+                  <p class="card-text">{{$movie->plot}}</p>
+                  <a href="{{route('movies.show', $movie->id)}}" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
+
+@endsection
