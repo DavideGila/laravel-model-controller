f1406b8d7b5ac4b5fde1c5ac8e831cfcdd237990
DavideGila <davide.gila@gmail.com>
2023-12-20 15:54:52 +0100

    added navbar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,17 @@
-<div>Header</div>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="{{route('home')}}">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{route('movies.index')}}">Movies</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+</header>
